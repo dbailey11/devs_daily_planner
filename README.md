2ba67eaae0c597498b339e5ac101b0c6d698d414
Devyn Bailey <devynbailey11@yahoo.com>
2020-10-25 20:50:43 -0600

    added README.md, edited comments in js file, put cs and js into assets folder along with screenshot image.

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# devs_daily_planner
+# Daily Planner
+
+## Purpose
+
+The purpose of this project was to get more experience working with Javascript as well as local storage and Moment.js. Moment.js was used in this application to display the current date. It was also used to display certain block colors depending on the current time. Within this application you are able to enter in an event on any time block you wish and it will save into the local storage even if you leave the page and come back. The applicaiton also color codes the time blocks depending on the day so you can keep track of the past, present and future events. The practical aspect is to be able to track any daily meetings/events/to-do's a user might need throughout their work day and have it save until they would like to add something different.
+
+### Screenshots
+
+![Image of password generator application](assets\images\devs_daily_planner_screenshot.png)
+
+## Installation
+
+To access the daily planner, please click on the leak below. No specific requirements to run application. Will need a code editing software such as VS Coding Studio to work on any updates or additions.
+
+## Skills
+
+![badges](https://img.shields.io/badge/<SKILLS>-<JAVASCRIPT>-informational?style=flat&logo=<LOGO_NAME>&logoColor=white&color=2bbc8a)
+![badges](https://img.shields.io/badge/<SKILLS>-<MOMENT.JS>-informational?style=flat&logo=<LOGO_NAME>&logoColor=white&color=2bbc8a)
+
+## Resources (Documentation and other links)
+
+- "Javascript & JQuery, interactive front-end web development" - Jon Duckett
+- Class mates: Mychael Martinez, Michael Li, Robby Ray
+- Friend: Tanner May - a CSU student interning as a developer
+- Websites:
+  - w3schools.com
+  - https://stackoverflow.com/questions/1497481/javascript-password-generator
+  - api.jquery.com
+
+## License
+
+No license as this is a reference for others out there just getting started learning Javascript.
---
+++ assets/script.js
@@ -0,0 +1,92 @@
+
+//adding current date and time via moment.js
+let m = moment();
+// console.log("hi", m);
+
+//displaying current date
+$("#currentDay").text(m.format('MMMM Do YYYY')); 
+
+
+//saving to local storage
+
+var userInput = document.getElementsByClassName('.description');
+var saveBtn = document.getElementsByClassName(".saveBtn");
+var hours = ["8", "9", "10", "11", "12", "1", "2", "3", "4", "5"]
+
+//function for when page loads, any previous user input will be displayed
+window.onload = function () {
+    // console.log("hello");
+
+    for(i = 0; i < hours.length; i++) {
+        //checking to see if there is something is in localstorage for certain time block row - checking the key
+        if (localStorage.getItem(hours[i]) != null) {
+           
+           var row = document.getElementById('text' + hours[i]);
+           row.value = localStorage.getItem(hours[i]); 
+        }
+    }
+}
+
+//button functionality and saving to local storage
+function saveRow (buttonName) {
+    // console.log("D", buttonName);
+
+    //using substring from index 3 to the end to get the number
+    var number = buttonName.substring(3, buttonName.length);
+    // console.log("E", number);
+
+    // make sure to get correct time block row
+    var row = document.getElementById('text' + number);
+
+    //saves input to local storage as long as there is some sort of input there
+    if(row.value != null) {
+        localStorage.setItem(number, row.value);
+    }
+}
+
+//time block row background color update
+
+
+
+
+function hourUpdate() {
+
+
+    var thisHour = parseInt(moment().format("HH"));
+    
+    //testing different times
+    // var thisHour = 12;
+    // console.log("F", thisHour);
+
+    var present = document.getElementById(thisHour);
+    // var past = document.getElementById(thisHour - 1);
+        
+    //setting the color for the present time block row
+    present.classList.add('present');
+    //checking if present contains class past/future and remove those
+    if(present.classList.contains('past')) {
+        present.classList.remove('past');
+    }
+    if(present.classList.contains('future')) {
+        present.classList.remove('future');
+    }
+        
+    //for loops that check the current time to make sure the correct color is displayed for each time block row - removes the color that doesn't belong
+    for(i = thisHour; i <= 17; i++) {
+        document.getElementById(i).classList.add('future');
+        if(document.getElementById(i).classList.contains('past')) {
+            //removes the past class if it contains it
+            document.getElementById(i).classList.remove('past');
+        }
+    }
+
+    for(i = 8; i <= thisHour; i++) {
+        document.getElementById(i).classList.add('past');
+        if(document.getElementById(i).classList.contains('future')) {
+            //removes the future class if it contains it
+            document.getElementById(i).classList.remove('future');
+        }
+    }
+}
+hourUpdate();
+// console.log("C", hourUpdate);

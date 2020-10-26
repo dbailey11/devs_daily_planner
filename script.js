e3438fe4403a2390a159c29ce746e81bfda6f720
Devyn Bailey <devynbailey11@yahoo.com>
2020-10-25 20:08:34 -0600

    edited html rows by adding onclick and additional id's, changed to military time, edited js to save input and add background color

--- script.js
+++ script.js
@@ -1,60 +1,96 @@
 
 //adding current date and time via moment.js
 let m = moment();
-console.log("hi", m);
+// console.log("hi", m);
 
-$("#currentDay").text(m.format('MMMM Do YYYY')); //displaying current date
+//displaying current date
+$("#currentDay").text(m.format('MMMM Do YYYY')); 
 
 
 //saving to local storage
 
 var userInput = document.getElementsByClassName('.description');
 var saveBtn = document.getElementsByClassName(".saveBtn");
+var hours = ["8", "9", "10", "11", "12", "1", "2", "3", "4", "5"]
 
+window.onload = function () {
+    // console.log("hello");
 
-//saving event function
-function setDailySchedule() {
-
-    userInput = localStorage.getItem("description").value;
-    console.log("A", userInput);
-
-
-}
-setDailySchedule();
-console.log("bye", setDailySchedule);
-
-//save button function
-saveBtn.onclick = function () {
-
-    localStorage.setItem("description", userInput.val);
+    for(i = 0; i < hours.length; i++) {
+        //checking to see if there is something is in localstorage for time - checking the key
+        if (localStorage.getItem(hours[i]) != null) {
+           
+           var row = document.getElementById('text' + hours[i]);
+           row.value = localStorage.getItem(hours[i]); 
+        }
+    }
 
 }
-console.log("B", saveBtn);
+
+
+//give button functionality 
+function saveRow (buttonName) {
+    // console.log("D", buttonName);
+
+    //using substring from index 3 to the end to get the number
+    var number = buttonName.substring(3, buttonName.length);
+    // console.log("E", number);
+    // make sure to get correct time row
+    var row = document.getElementById('text' + number);
+
+    if(row.value != null) {
+        localStorage.setItem(number, row.value);
+    }
+
+}
 
 
 //time of day row background color update
 
-var hours = ["8", "9", "10", "11", "12", "1", "2", "3", "4", "5"]
-console.log(hours);
-hourUpdate();
 
+
+hourUpdate();
 
 //use .querySelector for css classes?
 
 function hourUpdate() {
-    
-        var thisHour = parseInt(moment().format("HH"));
 
-    for(var i = 0; i < hours.length; i++) {
-        if (thisHour > hours) {
-            $(this).addClass("future")
+    // var hoursMil = ["8", "9", "10", "11", "12", "13", "14", "15", "16", "17"]
+
+        var thisHour = parseInt(moment().format("HH"));
+       
+        // var thisHour = 10;
+        // console.log("F", thisHour);
+
+        var present = document.getElementById(thisHour);
+        // var past = document.getElementById(thisHour - 1);
+        
+        //
+        present.classList.add('present');
+        //checking if present contains class past/future and remove those
+        if(present.classList.contains('past')) {
+            present.classList.remove('past');
         }
-        else if (thisHour === hours) {
-            $(this).addClass("present");
+        if(present.classList.contains('future')) {
+            present.classList.remove('future');
         }
-        else {
-            $(this).addClass("past");
+        
+
+        for(i = thisHour; i <= 17; i++) {
+            document.getElementById(i).classList.add('future');
+            if(document.getElementById(i).classList.contains('past')) {
+                //removes the past class if it contains it
+                document.getElementById(i).classList.remove('past');
+            }
         }
-    }
+
+        for(i = 8; i <= thisHour; i++) {
+            document.getElementById(i).classList.add('past');
+            if(document.getElementById(i).classList.contains('future')) {
+                //removes the future class if it contains it
+                document.getElementById(i).classList.remove('future');
+            }
+        }
+ 
 }
-console.log("C", hourUpdate);
+// console.log("C", hourUpdate);
